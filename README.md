2e6b10a1e87ae052051ffe841446c2a5446d2df2
Dominik Moritz <domoritz@gmail.com>
2012-11-11 16:44:24 +0100

    Add readme, layer implementation and quad tree

--- README.md
+++ README.md
@@ -1,4 +1,65 @@
-leaflet-maskcanvas
-==================
+#Leaflet MaskCanvas
 
-A leaflet canvas layer for displaying large coverage data sets
+A leaflet canvas layer for displaying large coverage data sets.
+
+__Features__:
+
+* Canvas tile layer based
+* High performance even for large dataset because of the [QuadTree](https://en.wikipedia.org/wiki/Quadtree) that is used internally
+* Custom color and circle size
+
+# Demo
+
+Not yet available.
+
+#Usage
+
+## Set up
+
+* Add the MaskCanvas and Quadtree libraries.
+
+```html
+<script src="QuadTree.js"></script>
+<script src="L.TileLayer.MaskCanvas.js"></script>
+```
+
+* Initialize the maskCanvas layer
+
+```javascript
+L.TileLayer.maskCanvas();
+```
+
+* Set the dataset for the layer.
+
+```javascript
+layer.setData(data);
+```
+
+* Finally add the layer to the map.
+
+```javascript
+map.addLayer(layer);
+```
+
+The data format is a simple array of `[lat, lng]` pairs.
+
+For example `[[51.503076,-0.280115],[51.51412,-0.075334],[51.5154,-0.072642],[51.54072,-0.299246]]`. I recommend that you load the data set asynchronously in order to keep the page responsive. Once the data is loaded, you can add it to the layer and display it.
+
+## Possible options
+
+The MaskCanvas layer supports all [Leaflet canvas layer options](http://leafletjs.com/reference.html#tilelayer-options) which can be passed to `L.TileLayer.maskCanvas`. You probably want to set the layer opacity.
+
+Other possible options:
+
+```javascript
+options: {
+       radius: 5,  // radius of a masked circle around a data point
+       color: '#000'  // the color of the layer
+}
+```
+
+# Screenshot
+
+# Acknowledgement
+
+The QuadTree implementation comes from https://github.com/jsmarkus/ExamplesByMesh/tree/master/JavaScript/QuadTree and has been slightly modified. Original Implementation by Mike Chambers.
---
+++ src/L.TileLayer.MaskCanvas.js
@@ -0,0 +1,133 @@
+L.TileLayer.MaskCanvas = L.TileLayer.Canvas.extend({
+	options: {
+        radius: 5,
+        color: '#000',
+        opacity: 0.5,
+        debug: false
+	},
+
+	initialize: function (options, data) {
+        var self = this;
+        L.Util.setOptions(this, options);
+
+        this.drawTile = function (tile, tilePoint, zoom) {
+            var ctx = {
+                canvas: tile,
+                tilePoint: tilePoint,
+                zoom: zoom
+            };
+
+            if (self.options.debug) {
+                self._drawDebugInfo(ctx);
+            }
+            this._draw(ctx);
+        };
+    },
+
+    _drawDebugInfo: function (ctx) {
+        var max = this.tileSize;
+        var g = ctx.canvas.getContext('2d');
+        g.globalCompositeOperation = 'destination-over';
+        g.strokeStyle = '#000000';
+        g.fillStyle = '#FFFF00';
+        g.strokeRect(0, 0, max, max);
+        g.font = "12px Arial";
+        g.fillRect(0, 0, 5, 5);
+        g.fillRect(0, max - 5, 5, 5);
+        g.fillRect(max - 5, 0, 5, 5);
+        g.fillRect(max - 5, max - 5, 5, 5);
+        g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
+        g.strokeText(ctx.tilePoint.x + ' ' + ctx.tilePoint.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
+    },
+
+    _createTile: function () {
+        var tile = this._canvasProto.cloneNode(false);
+        tile.onselectstart = tile.onmousemove = L.Util.falseFn;
+
+        var tileSize = this.options.tileSize;
+        var g = tile.getContext('2d');
+        g.fillStyle = this.options.color;
+        g.fillRect(0, 0, tileSize, tileSize);
+        g.globalCompositeOperation = 'destination-out';
+        return tile;
+    },
+
+    setData: function(dataset) {
+        var self = this;
+
+        this.bounds = new L.LatLngBounds(dataset);
+
+        this._quad = new QuadTree(this._boundsToQuery(this.bounds), false, 100);
+
+        dataset.forEach(function(d) {
+            self._quad.insert({
+                x: d[1], //lng
+                y: d[0] //lat
+            });
+        });
+        this.redraw();
+    },
+
+    _tilePoint: function (ctx, coords) {
+        // start coords to tile 'space'
+        var s = ctx.tilePoint.multiplyBy(this.options.tileSize);
+
+        // actual coords to tile 'space'
+        var p = this._map.project(new L.LatLng(coords[0], coords[1]));
+
+        // point to draw
+        var x = Math.round(p.x - s.x);
+        var y = Math.round(p.y - s.y);
+        return [x, y];
+    },
+
+    _drawPoints: function (ctx, coordinates) {
+        var c = ctx.canvas;
+        var g = c.getContext('2d');
+        var self = this;
+        var p;
+        coordinates.forEach(function(coords){
+            p = self._tilePoint(ctx, coords);
+            g.beginPath();
+            g.arc(p[0], p[1], self.options.radius, 0, Math.PI * 2);
+            g.fill();
+        });
+    },
+
+    _boundsToQuery: function(bounds) {
+        return {
+            x: bounds.getSouthWest().lng,
+            y: bounds.getSouthWest().lat,
+            width: bounds.getNorthEast().lng-bounds.getSouthWest().lng,
+            height: bounds.getNorthEast().lat-bounds.getSouthWest().lat
+        };
+    },
+
+    _draw: function (ctx) {
+        var tileSize = this.options.tileSize;
+
+        var nwPoint = ctx.tilePoint.multiplyBy(tileSize);
+        var sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
+
+        // padding
+        var pad = new L.Point(this.options.radius, this.options.radius);
+        nwPoint = nwPoint.subtract(pad);
+        sePoint = sePoint.add(pad);
+
+        var bounds = new L.LatLngBounds(this._map.unproject(sePoint), this._map.unproject(nwPoint));
+
+        var coordinates = [];
+        this._quad.retrieveInBounds(this._boundsToQuery(bounds)).forEach(function(obj) {
+            coordinates.push([obj.y, obj.x]);
+        });
+
+        this._drawPoints(ctx, coordinates);
+
+        var c = ctx.canvas;
+        var g = c.getContext('2d');
+    }
+});
+
+L.TileLayer.maskCanvas = function (options) {
+    return new L.TileLayer.MaskCanvas(options);
+};
